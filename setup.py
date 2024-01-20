ede6961a7eeac05779b3b4949fb7808365a5662d
Sai Chaitanya Nandipati <snandipati3@gatech.edu>
2024-01-19 22:52:52 -0800

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Detect and classify seizures and other types of harmful brain activity',
+    author='Sai Chaitanya Nandipati',
+    license='MIT',
+)
